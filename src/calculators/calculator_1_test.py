778980094f1c08a884a1eeeabd7aa7102e5f8cc6
soapmactavish23 <ricknogueira1231@gmail.com>
2026-01-27 23:25:25 -0300

    Primeiro Teste Unitário

--- src/calculators/calculator_1_test.py
+++ src/calculators/calculator_1_test.py
@@ -1,2 +1,23 @@
-def test_something():
-    pass
+from typing import Dict
+
+from src.calculators.calculator_1 import Calculator1
+
+
+class MockRequest:
+    def __init__(self, body: Dict) -> None:
+        self.json = body
+
+def test_calculate():
+    mock_request = MockRequest(body={"number": 1})
+    calculator_1 = Calculator1()
+
+    response = calculator_1.calculate(mock_request)
+
+    # Formato da resposta
+    assert "data" in response
+    assert "Calculator" in response["data"]
+    assert "result" in response["data"]
+
+    # Assertividade da Resposta
+    assert response["data"]["result"] == 14.25
+    assert response["data"]["Calculator"] == 1
