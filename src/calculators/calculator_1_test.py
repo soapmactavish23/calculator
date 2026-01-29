fcfae04c9ba48363966a48eb246243318328db81
soapmactavish23 <ricknogueira1231@gmail.com>
2026-01-28 23:07:49 -0300

    Testando Erros

--- src/calculators/calculator_1_test.py
+++ src/calculators/calculator_1_test.py
@@ -1,5 +1,5 @@
 from typing import Dict
-
+from pytest import raises
 from src.calculators.calculator_1 import Calculator1
 
 
@@ -21,3 +21,12 @@ def test_calculate():
     # Assertividade da Resposta
     assert response["data"]["result"] == 14.25
     assert response["data"]["Calculator"] == 1
+
+def test_calculate_with_body_error():
+    mock_request = MockRequest(body={"something": 1})
+    calculator_1 = Calculator1()
+
+    with raises(Exception) as excinfo:
+        calculator_1.calculate(mock_request)
+
+    assert str(excinfo.value) == "body mal formatado!"
