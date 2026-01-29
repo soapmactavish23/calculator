7572c40bf4196fc5b798a0554f782df4df4e27ba
soapmactavish23 <ricknogueira1231@gmail.com>
2026-01-28 23:18:24 -0300

    Iniciando Lógica da Segunda Calculadora

---
+++ src/calculators/calculator_2_test.py
@@ -0,0 +1,12 @@
+from typing import Dict
+from .calculator_2 import Calculator2
+
+class MockRequest:
+    def __init__(self, body: Dict) -> None:
+        self.json = body
+
+def test_calculate():
+    mock_request = MockRequest({"numbers": [2.12, 4.62, 1.32]})
+
+    calculator_2 = Calculator2()
+    calculator_2.calculate(mock_request)
