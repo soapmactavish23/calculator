bb2bb2a8f8e7d08980c71c4449406188a46d9fe5
soapmactavish23 <ricknogueira1231@gmail.com>
2026-02-10 20:40:37 -0300

    Terminando Lógica da Terceira Calculadora

--- src/calculators/calculator_3.py
+++ src/calculators/calculator_3.py
@@ -8,4 +8,42 @@ class Calculator3:
         self.__driver_handler = driver_handler
 
     def calculate(self, request: FlaskRequest) -> Dict:
-        pass
+        body = request.json
+        input_data = self.__validate_body(body)
+
+        variance = self.__calculate_variance(input_data)
+        multiplication = self.__calculate_multiplication(input_data)
+        self.__verify_results(variance, multiplication)
+        formated_response = self.__format_response(multiplication)
+        return formated_response
+
+
+    def __validate_body(self, body: Dict) -> List[float]:
+        if "numbers" not in body:
+            raise Exception("body mal formatado!")
+
+        input_data = body["numbers"]
+        return input_data
+
+    def __calculate_variance(self, numbers: List[float]) -> float:
+        variance = self.__driver_handler.variance(numbers)
+        return variance
+
+    def __calculate_multiplication(self, numbers: List[float]) -> float:
+        multiplication = 1
+        for num in numbers: multiplication *= num
+
+        return multiplication
+
+    def __verify_results(self, variance: float, multiplication: float) -> None:
+        if variance < multiplication:
+            raise Exception("Falha no processo: Variância menor que multiplicação")
+
+    def __format_response(selfself, variance: float) -> Dict:
+        return {
+            "data": {
+                "Calculator": 3,
+                "value": variance,
+                "Success": True
+            }
+        }
